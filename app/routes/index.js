5df88b36a585820344139d690e157cdd5ed6570e
Weiwei Zhong <cspilgrimzww@163.com>
2016-07-21 22:58:07 +0800

    temp commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', {
-    title: 'Express' ,
-    // layout:false
+  res.render('allList', {
+    title: 'Home'
   });
+});
+router.get('/home', function(req, res, next) {
+  res.render('allList', {
+    title: 'Home'
+  });
+});
+router.get('/raised', function(req, res, next) {
+  res.render('raisedList', {
+    title: 'Home'
+  });
+});
+router.get('/founded', function(req, res, next) {
+  res.render('foundedList', {
+    title: 'Home'
+  });
+});
+
+router.get('/newProj',function (req,res,next) {
+  res.render('newProj');
 });
 
 module.exports = router;
--- app/routes/project.js
+++ app/routes/project.js
@@ -5,8 +5,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/raisedDetail', function(req, res, next) {
+    var from = req.params.from;
+    console.log(from);
+    res.render('raisedDetail',
+        {
+            title:"raisedDetail"
+        });
+});
 router.get('/projDetail', function(req, res, next) {
-    res.render('projDetail');
+    var from = req.params.from;
+    console.log(from);
+    res.render('projDetail',
+        {
+            title:"ProjDetail"
+        });
+});
+router.get('/foundedDetail', function(req, res, next) {
+    var from = req.params.from;
+    console.log(from);
+    res.render('foundedDetail',
+        {
+            title:"foundedDetail"
+        });
 });
 
 module.exports = router;
